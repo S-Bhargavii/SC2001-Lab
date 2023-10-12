e7044670d4a784252943ed3eb992494ece955988
Vignesh-Tharun <130673203+Vignesh-Tharun@users.noreply.github.com>
2023-10-12 22:48:58 +0800

    Create PriorityQueueArray.java
    Added implementation of priority queue using array

---
+++ Lab 2/PriorityQueueArray/PriorityQueueArray.java
@@ -0,0 +1,34 @@
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] weightOfEdges = {7, 3, 4, 8, 1, 6, 9, 2, 5, 10};
+
+        //--Priority queue using array--
+
+        PriorityQueueArray priorityQueueArray = new PriorityQueueArray();
+
+        for (int i = 0; i < weightOfEdges.length; i++) {
+            priorityQueueArray.enqueue(weightOfEdges[i]);
+        }
+
+        priorityQueueArray.dequeue();
+        int minimum = priorityQueueArray.dequeue();
+        System.out.println(minimum);
+
+        //--Priority queue using min heap--
+
+        //By default, Java's native priority queue by default uses min heap internally
+        PriorityQueue<Integer> priorityQueueHeap = new PriorityQueue<Integer>();
+
+        //Use add method to insert integers to priority queue
+        for (int i = 0; i < weightOfEdges.length; i++) {
+            priorityQueueHeap.add(weightOfEdges[i]);
+        }
+
+        //Use poll method to retrieve the smallest element from the queue
+        //Null when no elements present in queue
+        int min = priorityQueueHeap.poll();
+        System.out.println(min);
+    }
+}
